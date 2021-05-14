4a7bd507ace8b7cdd091d72d48e99153cf703d73
anil-angular <donwade.anil@gmail.com>
2021-05-15 01:59:36 +0530

    4 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-  @Input() product;
+  
   constructor() { }
 
   ngOnInit() {
