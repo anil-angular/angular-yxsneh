641862fb26318464ba7ff181db02b2828b5d49b9
anil-angular <donwade.anil@gmail.com>
2021-05-15 01:24:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-yxsneh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-yxsneh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() product;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
